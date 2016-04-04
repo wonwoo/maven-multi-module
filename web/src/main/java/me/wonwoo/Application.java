d7a6f0a91fcbc1405568fc5f3c5da961c5387345
wonwoo <aoruqjfu@gmail.com>
2016-04-04 22:48:03 +0900

    Initial commit

--- web/src/main/java/me/wonwoo/Application.java
+++ web/src/main/java/me/wonwoo/Application.java
@@ -2,6 +2,7 @@ package me.wonwoo;
 
 import me.wonwoo.entity.Account;
 import me.wonwoo.repository.AccountRepository;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -29,6 +30,5 @@ public class Application {
         new Account("wonwoo", "123"),
         new Account("youngjin", "0000")
       ).forEach(accountRepository::save);
-
   }
 }
