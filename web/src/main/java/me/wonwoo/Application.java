ff77e686a63041a6a4693535c9ad70d373511f44
wonwoo <aoruqjfu@gmail.com>
2016-04-04 22:48:14 +0900

    Initial commit

--- web/src/main/java/me/wonwoo/Application.java
+++ web/src/main/java/me/wonwoo/Application.java
@@ -2,7 +2,6 @@ package me.wonwoo;
 
 import me.wonwoo.entity.Account;
 import me.wonwoo.repository.AccountRepository;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
